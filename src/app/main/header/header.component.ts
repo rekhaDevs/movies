d012b58be3df00db69c82dc9cfde70913ffa7a2d
Rekha <rekha.sharma@luezoid.com>
2023-08-06 23:22:32 +0530

    UI

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { StorageService } from 'src/app/shared/services/storage.service';
 import { ThemeToggleService } from 'src/app/shared/services/theme-toggle.service';
 
 @Component({
@@ -7,15 +9,32 @@ import { ThemeToggleService } from 'src/app/shared/services/theme-toggle.service
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  isDark: any;
   constructor(
-    private themeToggleService: ThemeToggleService
+    private themeToggleService: ThemeToggleService,
+    private storageService: StorageService,
+    private router : Router
   ){}
 
   ngOnInit()  {
-
+    this.checkingTheme();
   }
 
   toggleDarkMode() {
     this.themeToggleService.toggleMode();
+  }
+
+  checkingTheme(){
+    if(StorageService.getItem('theme') == 'dark'){
+      this.isDark = true;
+    }
+    if(StorageService.getItem('theme') == 'light'){
+      this.isDark = false;
+    }
+  }
+
+  logOut(): void {
+    StorageService.clearAll();
+    this.router.navigateByUrl('login');
   }
 }
