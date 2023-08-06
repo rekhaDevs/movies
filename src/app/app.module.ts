20bfe5135042993166adbcbae2c1ecf8a97ac778
Rekha <rekha.sharma@luezoid.com>
2023-08-07 01:45:52 +0530

    minorUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {StorageService} from "./shared/services/storage.service";
 import { HeaderComponent } from './main/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     HttpClientModule,
     NgToastModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatPaginatorModule
   ],
   providers: [
     StorageService,
--- src/app/pages/movies/movies-list/movies-list.component.ts
+++ src/app/pages/movies/movies-list/movies-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterTestingHarness } from '@angular/router/testing';
 import { MoviesService } from 'src/app/shared/http-services/movies.service';
+import { AlertService } from 'src/app/shared/services/alert-service.service';
 
 @Component({
   selector: 'app-movies-list',
@@ -8,8 +10,16 @@ import { MoviesService } from 'src/app/shared/http-services/movies.service';
 })
 export class MoviesListComponent  implements OnInit{
   movieList: Array<any> = [];
+  isActive: boolean =true;
+  paginate = {
+    pageIndex: 0,
+    pageSize: 10,
+    total: 1
+  };
+  currentPage:number = 1;
   constructor(
-    private movieService: MoviesService
+    private movieService: MoviesService,
+    private alert : AlertService
   ){
 
   }
@@ -23,12 +33,22 @@ export class MoviesListComponent  implements OnInit{
       (data)=>{
         if(data){
           this.movieList = data.results;
-          console.log("jhjhjh", this.movieList);
-          
+          this.isActive = false;          
         }
+
+      },
+      (error)=>{
+        this.isActive = false;          
+        this.alert.popToaster('error', 'Try Again', error?.error?.error?.message, {duration: 5000} )
 
       }
     )
+  }
+
+  onPageChange(event: any) {
+    console.log("hhh",event);
+    this.currentPage = event.pageIndex;
+    this.getMovieList();
   }
 
 }
