34fdb00d6565997aee9e9ef328de2fd50b61b71b
Rekha <rekha.sharma@luezoid.com>
2023-08-06 21:11:04 +0530

    authGuard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MoviesListComponent } from './pages/movies/movies-list/movies-list.component';
 import { UserLoginComponent } from './pages/login/user-login.component';
+import { AuthGuard } from './shared/guards/auth-guard.service';
 
 const routes: Routes = [
   {path: '', component: UserLoginComponent},
   {path: 'login', component: UserLoginComponent},
-  {path: 'movies', component: MoviesListComponent},
+  {path: 'movies', component: MoviesListComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeToggleService } from 'src/app/shared/services/theme-toggle.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(
+    private themeToggleService: ThemeToggleService
+  ){}
 
+  ngOnInit()  {
+
+  }
+
+  toggleDarkMode() {
+    this.themeToggleService.toggleMode();
+  }
 }
