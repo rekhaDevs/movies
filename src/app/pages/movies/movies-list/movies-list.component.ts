7b4a0cd732c3415efce5b3593f6a99153dd04162
Rekha <rekha.sharma@luezoid.com>
2023-08-07 02:33:23 +0530

    paginator UI

--- src/app/pages/movies/movies-list/movies-list.component.ts
+++ src/app/pages/movies/movies-list/movies-list.component.ts
@@ -13,8 +13,9 @@ export class MoviesListComponent  implements OnInit{
   isActive: boolean =true;
   paginate = {
     pageIndex: 0,
-    pageSize: 10,
-    total: 1
+    pageSize: 9,
+    length: 10
+   
   };
   currentPage:number = 1;
   constructor(
@@ -25,19 +26,27 @@ export class MoviesListComponent  implements OnInit{
   }
 
   ngOnInit(): void {
-    this.getMovieList();
+    this.getMovieList(this.paginate.pageIndex +1);
   }
 
-  getMovieList(){
-    this.movieService.getMovieList({}).subscribe(
+  getMovieList(params: any){
+    this.movieService.getMovieList(params).subscribe(
       (data)=>{
         if(data){
           this.movieList = data.results;
-          this.isActive = false;          
+          this.isActive = false; 
+          this.paginate = {
+            pageIndex: this.paginate.pageIndex ,
+            pageSize: 10,
+            length: data.count,
+          };       
+          console.log("this.move", this.paginate.pageIndex, this.movieList);
+            
         }
 
       },
       (error)=>{
+        this.paginate.pageIndex = params;
         this.isActive = false;          
         this.alert.popToaster('error', 'Try Again', error?.error?.error?.message, {duration: 5000} )
 
@@ -48,7 +57,7 @@ export class MoviesListComponent  implements OnInit{
   onPageChange(event: any) {
     console.log("hhh",event);
     this.currentPage = event.pageIndex;
-    this.getMovieList();
+    this.getMovieList(event.pageIndex + 1);
   }
 
 }
